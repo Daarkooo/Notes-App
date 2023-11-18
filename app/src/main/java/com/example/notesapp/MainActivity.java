ee4f3f2970d1570e5f408bf97abdcc03ea303a76
Daarkooo <121336377+darkoo16@users.noreply.github.com>
2023-11-18 01:16:43 +0100

    added first part

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,6 +1,18 @@
 package com.example.notesapp;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import com.example.notesapp.Adapters.NotesListAdpater;
+import com.example.notesapp.Datebase.RoomDB;
+import com.example.notesapp.Models.Notes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -14,12 +26,73 @@ import com.example.notesapp.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    NotesListAdpater notesListAdpater;
+    List<Notes> notes =new ArrayList<>();
+    RoomDB database;
+    FloatingActionButton fab_add;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+        database = RoomDB.getInstance(this);
+        notes = database.mainDAO().getAll();
+
+        updateRecycler(notes);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
     }
+
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==101){
+            if (resultCode == Activity.RESULT_OK){
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                database.mainDAO().insert(new_notes);
+                notes.clear();
+                notes.addAll(database.mainDAO().getAll());
+                notesListAdpater.notifyDataSetChanged();
+            }
+        }
+    }
+
+    private void updateRecycler(List<Notes> notes) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        notesListAdpater = new NotesListAdpater(MainActivity.this, notes, notesClickListener );
+        recyclerView.setAdapter(notesListAdpater);
+
+    }
+
+    private final NotesClickListener notesClickListener= new NotesClickListener() {
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
+    };
 }
 
